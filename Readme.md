e273ae0b5b207de11045dc71fec842ef4f7777f1
acgessler <alexander.gessler@gmx.net>
2012-01-19 18:43:55 +0100

    - basic evaluation and substitution kernel working.

--- Readme.md
+++ Readme.md
@@ -22,8 +22,7 @@ to this page is also fine).
 ```javascript
 
 // setup basic settings for the preprocessor. The values below
-// are the default values, so you can omit them if you're fine
-// with them.
+// are the default values, you an omit them if you agree.
 var settings = { 
 
    // character / string that starts a preprocessor command, 
@@ -32,22 +31,54 @@ var settings = {
 }
 
 // create an instance of the library. `settings` may also be
-// omitted to use default values everywhere.
-var cpp = cpp_js( settings );
+// omitted at all to use only default values.
+var pp = cpp_js( settings );
 
 // predefine some symbols, the same effect could be reached
 // by prepending the corresponding #define's to the source
-// code to be preprocessed but this is way nicer.
+// code for preprocessing but this is way nicer.
+// cpp.js by itself does not predefine any symbols.
 var predefined = {
    DEBUG : '',               // equivalent to `#define DEBUG`
    ANOTHER_DEFINE : '248935' // equivalent to `#define ANOTHER_DEFINE 248935`
 };
 
-// and invoke the preprocesser on the given text. The returned source
-// no longer contains any preprocessor commands and input strings 
+pp.define_multiple(predefined);
+
+// you can also do it step by step
+pp.define('UNIVERSAL_TRUTH','42');
+pp.undef('DEBUG');
+
+// and query the current state of a particular define
+pp.defined('DEBUG'); // => false
+
+// now invoke the preprocesser on the given text. The returned source
+// no longer contains any preprocessor commands. Note: input strings 
 // containing not a single preprocessor command are returned unchanged, 
-// i.e. cpp.run(cpp.run(text, ...)) == cpp.run(text, ...)
-var preprocessed_source = cpp.run(text, predefined);
+// i.e. pp.run(pp.run(text, ...)) == pp.run(text, ...)
+
+// note that the processor keeps the state obtained from executing 
+// the text block, so if run() is invoked on multiple text blocks,
+// any defines from a block will also be available to its successors.
+var preprocessed_source = pp.run(text);
+
+// calling clear() resets all defined values, so the effect of 
+// calling run() after clear()ing is the same as calling run()
+// on a fresh instance of cpp.js.
+pp.clear();
+
+```
+
+
+Running it from node.js is also possible:
+
+
+```javascript
+
+var cpp = require("./cpp");
+var pp = cpp.create( settings );
+
+// rest as above
 
 ```
 
---
+++ cpp.js
@@ -0,0 +1,279 @@
+/*
+
+cpp.js - Simple implementation of the C Preprocessor in Javascript
+
+Copyright (c) 2011, Alexander C. Gessler
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms, 
+with or without modification, are permitted provided that the 
+following conditions are met:
+
+* Redistributions of source code must retain the above
+  copyright notice, this list of conditions and the
+  following disclaimer.
+
+* Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+* Neither the name of the ASSIMP team, nor the names of its
+  contributors may be used to endorse or promote products
+  derived from this software without specific prior
+  written permission of the ASSIMP Development Team.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+function cpp_js(settings) {
+
+	// http://blog.stevenlevithan.com/archives/faster-trim-javascript
+	var trim = function (str) {
+		str = str.replace(/^\s+/, '');
+		for (var i = str.length - 1; i >= 0; i--) {
+			if (/\S/.test(str.charAt(i))) {
+				str = str.substring(0, i + 1);
+				break;
+			}
+		}
+		return str;
+	};
+
+	
+	var default_settings = {
+		signal_char : '#'
+	};
+	
+	// apply default settings
+	if (!settings) {
+		settings = {};
+		for(var k in default_settings) {
+			if (!(k in settings)) {
+				settings[k] = default_settings[k];
+			}
+		}
+	}
+
+	var block_re = new RegExp("^"+settings.signal_char+"(\\w+)[ \t]*(.*?)[ \t]*$","m");
+	
+	// (match identifiers according to 6.4.2.1, do not match 'defined')
+	var is_identifier_re = /\b(d(?!efined)|[a-ce-zA-Z_])\w*(?![0-9a-zA-Z_"])/g;
+	
+	// same, but checks if the entire string is an identifier
+	var is_identifier_only_re = /^(d(?!efined)|[a-ce-zA-Z_])\w*$/g;
+	
+	// defined <identifier>
+	var defined_no_parens_re = /defined\s+([a-zA-Z_]\w*)/g;
+	
+	// defined (<identifier>)
+	var defined_re = /defined\s*\((\s*[a-zA-Z_]\w*\s*)\)/g;
+	
+	// __defined_magic_<identifier>_ (a special sentinel value used to
+	// temporarily exclude operands to defined() from macro substitution.
+	var defined_magic_sentinel_re = /__defined_magic_([a-zA-Z_]\w*)_/;
+	
+	var state = {};
+	
+	return {
+	
+		clear : function() {
+			state = {};
+		},
+		
+		defined : function(k) {
+			return k in state;
+		},
+	
+		define : function(k,v) {
+			if (!this._is_identifier(k)) {
+				throw "cpp.js: not a valid preprocessor identifier: '" + k + "'";
+			}
+			state[k] = v || '';
+		},
+		
+		undefine : function(k,v) {
+			delete state[k];
+		},
+		
+		define_multiple : function(dict) {
+			for(var k in dict) {
+				this.define(k,dict[k]);
+			}
+		},
+		
+		subs : function(text) {
+			var new_text = text;
+			for (var k in state) {
+				new_text = new_text.replace(new RegExp("\\b"+k+"\\b"),state[k]);
+			}
+			
+			// keep substituting until no further substitutions are possible
+			return new_text == text ? new_text : this.subs(new_text);
+		}, 
+	
+		run : function(text) {
+			
+			var blocks = text.split(block_re);
+			console.log(blocks);
+			
+			var out = new Array(Math.floor(blocks.length/3) + 2), outi = 0;
+			for (var i = 0; i < out.length; ++i) {
+				out[i] = '';
+			}
+			
+			var ifs_nested = 0, ifs_failed = 0;
+			
+			for (var i = 0; i < blocks.length; ++i) {
+			
+				var elem = blocks[i];
+				switch(i % 3) {
+					// code line, apply macro substitutions and copy
+					// to output.
+					case 0:
+						out[outi++] = this.subs(elem);
+						break;
+					// preprocessor statement, such as ifdef, endif, ..
+					case 1:
+						command = elem;
+						break;
+					// the rest of the preprocessor line, this is where
+					// expression evaluation happens
+					case 2:
+						var done = true;
+						switch (command) {
+							case "ifdef":
+							case "ifndef":
+								if (!elem) {
+									// TODO
+								}
+								elem = "(defined " + elem + ")";
+								if(command == 'ifndef') {
+									elem = '!' + elem;
+								}
+								
+							case "if":
+								++ifs_nested;
+								if (ifs_failed > 0 || !this._eval(elem)) {
+									++ifs_failed;
+								}
+								break;
+								
+							case "endif":
+								if(--ifs_nested < 0) {
+									// TODO
+								}
+								if (ifs_failed > 0) {
+									--ifs_failed;
+								}
+								break;
+								
+							default:
+								done = ifs_failed > 0;
+						};
+
+						if(!done) {
+							switch (command) {
+								case "define":
+									var e = elem.split(/\s/,1);
+									this.define(trim(e[0]), e[1] ? trim(e[1]) : undefined);
+									break;
+									
+								case "undef":
+									this.undef(elem);
+									break;
+									
+								case "include":
+									// TODO
+									break;
+									
+								case "error":
+									// TODO
+									break;
+									
+								case "pragma":
+								default:
+									// TODO
+									break;
+							};
+						}
+						
+						// ignore block contents if we are still within a dead block
+						if (ifs_failed > 0) {
+							++i;
+						}
+						break;
+				} 
+			}
+			
+			if(ifs_nested > 0) {
+				// TODO
+			}
+			
+			console.log(out);
+			return out.join('\n');
+		},
+		
+		_is_identifier : function(identifier) {
+			// Note: important to use match() because test() would update
+			// the 'lastIndex' regexp property.
+			return !!identifier.match(is_identifier_only_re);
+		},
+		
+		_eval : function(val) {
+			// 6.10.1.2-3
+			
+			console.log('_eval: ' + val);
+			
+			// macro substitution - but do not touch unary operands to 'defined',
+			// this is done by substituting a safe sentinel value (which starts
+			// with two underscores and is thus reserved).
+			val = val.replace(defined_no_parens_re,'defined($1)');
+			
+			console.log('_eval: ' + val);
+			
+			val = val.replace(defined_re,' __defined_magic_$1_ ');
+			
+			console.log('_eval: ' + val);
+			
+			val = this.subs(val);
+		
+			// re-substitute defined() terms and quote the argument
+			val = val.replace(defined_magic_sentinel_re,'defined("$1")');
+			
+			console.log('eval: ' + val);
+			
+			// replace all remaining identifiers with '0'
+			val = val.replace(is_identifier_re,' 0 ');
+			
+			console.log('eval: ' + val);
+			
+			// bring defined() function into direct scope
+			var defined = this.defined;
+			
+			// what remains should be safe to use with eval().
+			var res = !!eval(val);
+			
+			console.log('res: ' + res);
+			return res;
+		},
+	};
+};
+
+// node.js interface
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports.create = cpp_js;
+}
+
+
+
